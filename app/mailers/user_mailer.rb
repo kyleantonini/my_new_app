2f76d7deef6080d8eb94360552318b7a28ddafc7
kyleantonini <kyleantonini@gmail.com>
2016-02-08 16:50:21 -0700

    working on corrections to user mailer preview

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
   def contact_form(email, name, message)
   	@message = message
   	mail(:from => email,
-  		:to =>  'app46040154@heroku.com',
+  		:to =>  'kyleantonini@gmail.com',
   	    :subject => "A new contact form message from #{name}")
   end
 end
--- app/test/mailers/previews/user_mailer_preview.rb
+++ app/test/mailers/previews/user_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
   def contact_form
-  	UserMailer.contact_form("kyleantonini@gmail.com", "Kyle", "Hello World!")
+  	UserMailer.contact_form("kyleantonini@nini.nini", "Kyle", "Hello World!")
   end
 end
