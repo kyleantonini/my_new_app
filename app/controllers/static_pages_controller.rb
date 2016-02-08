8d61f603fc87cec14cf002715bcd78cc0cdd9d9b
kyleantonini <kyleantonini@gmail.com>
2016-02-08 11:11:28 -0700

    Created email form and submission thank you page, configuredroutes and controller for email, enabled Sendgrid plugin from Heroku

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,13 @@ class StaticPagesController < ApplicationController
   	# @featured_product = Product.first
   	@products = Product.limit(3)
   end
+  def thank_you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message = params[:message]
+  	ActionMailer::Base.mail(:from => @email,
+  		:to => 'kyleantonini@gmail.com',
+  		:subject => "A new contact form message from #{@name}",
+  	    :body => @message).deliver_now
+  end
 end
