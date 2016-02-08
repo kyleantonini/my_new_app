c58673d0f2ad08434e40473318ff328414660f14
kyleantonini <kyleantonini@gmail.com>
2016-02-08 16:55:21 -0700

    working on corrections to user mailer preview

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,10 +7,10 @@ class StaticPagesController < ApplicationController
   	@name = params[:name]
   	@email = params[:email]
   	@message = params[:message]
-  	ActionMailer::Base.mail(:from => @email,
-  		:to => 'kyleantonini@gmail.com',
-  		:subject => "A new contact form message from #{@name}",
-  	    :body => @message).deliver_now
+#  	ActionMailer::Base.mail(:from => @email,
+#  		:to => 'kyleantonini@gmail.com',
+#  		:subject => "A new contact form message from #{@name}",
+#  	    :body => @message).deliver_now
   	UserMailer.contact_form(@email, @name, @message).deliver_now
   end
 end
