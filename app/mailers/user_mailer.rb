809fe6318833c2cf35603d0e93ad5e041476f951
kyleantonini <kyleantonini@gmail.com>
2016-02-25 15:00:00 -0700

    Added email upon user registration. Made user destroying only visible to admin.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,11 @@ class UserMailer < ApplicationMailer
   		:to =>  'kyleantonini@gmail.com',
   	    :subject => "A new contact form message from #{name}")
   end
+
+  def welcome(user)
+  	@appname = "Kyle's App"
+  	mail( :to => user.email,
+  		  :subject => "Welcome to #{@appname}!")
+  end
+
 end
